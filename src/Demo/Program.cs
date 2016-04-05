805ac4a5397c40fcb89246ee163da2d949e9ec77
Keith Dahlby <dahlbyk@gmail.com>
2016-04-04 19:18:55 -0500

    Basic OWIN demo

---
+++ src/Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Diagnostics;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:12345";
+            using (WebApp.Start<Startup>(url))
+            {
+                if (Environment.UserInteractive)
+                    Process.Start(url);
+
+                Console.ReadLine();
+            }
+
+        }
+
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+#if DEBUG
+                app.UseErrorPage();
+#endif
+
+                app.UseFunc(
+                    next =>
+                    async env =>
+                    {
+                        Console.WriteLine(env["owin.RequestPath"]);
+
+                        await next(env);
+                        return;
+                    });
+
+                app.UseWelcomePage("/");
+            }
+        }
+    }
+}
